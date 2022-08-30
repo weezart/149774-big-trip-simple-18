f03783cf068723b2147a7c23c06148c5f78f50a7
weezart <weezart@yandex.ru>
2022-08-30 12:47:59 +0300

    3.5 Правки после проверки линтером.

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -4,8 +4,7 @@ import EventEditView from '../view/event-edit-view.js';
 import EventAddView from '../view/event-add-view.js';
 import EventView from '../view/event-view.js';
 import {render} from '../render.js';
-import {getRandomInteger, getRandomizedReducedArray} from "../utils";
-import {getIntegerArray} from "../mock/offers";
+import {getRandomInteger, getRandomizedReducedArray} from '../utils';
 
 export default class BoardPresenter {
   #boardContainer = null;
