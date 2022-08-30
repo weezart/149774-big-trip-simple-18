37ef8bc792618bed068e8c29e247e02c9baf2609
weezart <weezart@yandex.ru>
2022-08-30 11:51:19 +0300

    3.2 Выделит показ точек маршрута в функцию. WIP

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -33,19 +33,25 @@ export default class BoardPresenter {
     render(new SortView(), this.#boardContainer);
     render(this.eventListComponent, this.#boardContainer);
 
-    const editTripDestination = this.#destinations.find((destination) => destination.id === this.#tripPoints[0].destination);
-    const editTripOffers = this.#offers.filter(({id}) => this.#tripPoints[0].offers.some((offerId) => offerId === id));
-    const editTripOfferTypesId = this.#offerTypes.find((offerType) => offerType.type === this.#tripPoints[0].type);
-    const editTripAvailableOffers = this.#offers.filter(({id}) => editTripOfferTypesId.offers.some((offerId) => offerId === id));
-    render(new EventEditView(this.#tripPoints[0], editTripDestination, editTripOffers, editTripAvailableOffers), this.eventListComponent.element);
+    // const editTripDestination = this.#destinations.find((destination) => destination.id === this.#tripPoints[0].destination);
+    // const editTripOffers = this.#offers.filter(({id}) => this.#tripPoints[0].offers.some((offerId) => offerId === id));
+    // const editTripOfferTypesId = this.#offerTypes.find((offerType) => offerType.type === this.#tripPoints[0].type);
+    // const editTripAvailableOffers = this.#offers.filter(({id}) => editTripOfferTypesId.offers.some((offerId) => offerId === id));
+    // render(new EventEditView(this.#tripPoints[0], editTripDestination, editTripOffers, editTripAvailableOffers), this.eventListComponent.element);
 
     // Временные функции для проверки работы формы создания
     render(this.eventAddComponent, this.eventListComponent.element);
     this.eventAddComponent.element.remove();
-    for (let i = 1; i < this.#tripPoints.length; i++) {
-      const tripDestination = this.#destinations.find((destination) => destination.id === this.#tripPoints[i].destination);
-      const tripOffers = this.#offers.filter(({id}) => this.#tripPoints[i].offers.some((offerId) => offerId === id));
-      render(new EventView(this.#tripPoints[i], tripDestination, tripOffers), this.eventListComponent.element);
+    for (let i = 0; i < this.#tripPoints.length; i++) {
+      this.#renderEvent(this.#tripPoints[i]);
     }
+  };
+
+  #renderEvent = (point) => {
+    const destination = this.#destinations.find((destinationsItem) => destinationsItem.id === point.destination);
+    const offers = this.#offers.filter(({id}) => point.offers.some((offerId) => offerId === id));
+    const eventComponent = new EventView(point, destination, offers);
+
+    render(eventComponent, this.eventListComponent.element);
   };
 }
