4864351c3e1cee8477988f73950e0ad6e69b80e0
weezart <weezart@yandex.ru>
2022-09-14 21:46:58 +0300

    7.8 Отрефакторит фильтрацию. WIP

--- src/main.js
+++ src/main.js
@@ -6,7 +6,13 @@ import OffersModel from './model/offers-model.js';
 import DestinationsModel from './model/destinations-model.js';
 import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
-import {generateEventFilter} from './mock/event-filter.js';
+
+const filters = [
+  {
+    type: 'everything',
+    name: 'EVERYTHING',
+  },
+];
 
 const siteHeaderElement = document.querySelector('.trip-main');
 const siteMainElement = document.querySelector('.trip-events');
@@ -14,12 +20,11 @@ const filtersElement = document.querySelector('.trip-controls__filters');
 
 const offersModel = new OffersModel();
 const destinationsModel = new DestinationsModel();
-const tripPointsModel = new PointsModel();
+const pointsModel = new PointsModel();
 const filterModel = new FilterModel();
 
-const boardPresenter = new BoardPresenter(siteMainElement, offersModel, destinationsModel, tripPointsModel);
-const eventFilters = generateEventFilter(tripPointsModel.points);
+const boardPresenter = new BoardPresenter(siteMainElement, offersModel, destinationsModel, pointsModel);
 
 render(new NewEventButtonView(), siteHeaderElement);
-render(new FiltersView(eventFilters), filtersElement);
+render(new FiltersView(filters, 'everything'), filtersElement);
 boardPresenter.init();
