47727e0130f979d524633f1811539cd4ea9c43f9
weezart <weezart@yandex.ru>
2022-09-26 21:05:27 +0300

    8.2.4 Добавит поддержку обратной связи в презентере доски

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -77,12 +77,15 @@ export default class BoardPresenter {
   #handleViewAction = (actionType, updateType, update) => {
     switch (actionType) {
       case UserAction.UPDATE_POINT:
+        this.#eventPresenter.get(update.id).setSaving();
         this.#pointsModel.updatePoint(updateType, update);
         break;
       case UserAction.ADD_POINT:
+        this.#eventNewPresenter.setSaving();
         this.#pointsModel.addPoint(updateType, update);
         break;
       case UserAction.DELETE_POINT:
+        this.#eventPresenter.get(update.id).setDeleting();
         this.#pointsModel.deletePoint(updateType, update);
         break;
     }
